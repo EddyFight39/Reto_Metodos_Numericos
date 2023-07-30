16227dfb55a28f77b4ba6301d0cffeaf916c9aa6
EddyFight39 <87662601+EddyFight39@users.noreply.github.com>
2023-07-30 16:24:14 -0500

    Update MetodosNumericos.py

--- MetodosNumericos.py
+++ MetodosNumericos.py
@@ -22,3 +22,41 @@ mapa.save("mapa.html")
 import webbrowser
 webbrowser.open("mapa.html")
 
+
+
+
+#VERSION 2
+
+import folium
+
+def obtener_coordenadas():
+    coordenadas_str = input("Ingresa las coordenadas (latitud, longitud): ")
+    lat_str, lng_str = coordenadas_str.split(',')
+    lat = float(lat_str.strip())
+    lng = float(lng_str.strip())
+    return lat, lng
+
+def crear_mapa(posicion_actual):
+    print(f"La posición ingresada es: {posicion_actual}")
+
+    # Crear un mapa utilizando folium
+    mapa = folium.Map(location=posicion_actual, zoom_start=10)
+
+    # Agregar un marcador en la posición actual
+    folium.Marker(posicion_actual, popup="Posición actual").add_to(mapa)
+
+    # Guardar el mapa en un archivo HTML
+    mapa.save("mapa.html")
+
+    # Abrir el archivo HTML en el navegador
+    import webbrowser
+    webbrowser.open("mapa.html")
+
+if __name__ == "__main__":
+    # Obtener la posición actual ingresando las coordenadas
+    lat, lng = obtener_coordenadas()
+    posicion_actual = (lat, lng)
+
+    # Crear el mapa
+    crear_mapa(posicion_actual)
+
